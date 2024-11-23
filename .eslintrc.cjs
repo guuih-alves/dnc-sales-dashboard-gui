4351e10f3decd38975a4e61a8b47fc6a3118ce18
Guilherme Alves <guilherme_alves_08@hotmsil.com>
2024-11-23 10:55:25 -0300

    First commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+// .eslintrc.cjs
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'eslint-config-prettier'
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+    ],
+    rules: {
+      // Adicione suas regras personalizadas aqui
+    },
+  };
