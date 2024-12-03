5aeeb9adc7eed41309e8c77dd0897c2eb1464b91
Guilherme Alves <guilherme_alves_08@hotmsil.com>
2024-12-02 23:01:33 -0300

    Fix types import4

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { DarkTheme, GlobalStyle, lighTheme } from './styles/'
+import { GlobalStyle, lighTheme } from './styles/'
 
 
 //Para aplicar os temas globalmente
