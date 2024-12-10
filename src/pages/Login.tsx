3dce2e8944dfe486447a53a166766b367826e981
Guilherme Alves <guilherme_alves_08@hotmsil.com>
2024-12-10 19:51:30 -0300

    Feat Form component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,7 @@
 
 import { Box, Container, Grid } from "@mui/material";
-import { BannerImage } from "@/components";
-
-
-
+import { BannerImage, FormComponent, Logo, StyledH1, Styledp } from "@/components";
+import { pxToRem } from "@/utils";
 
 function Login() {
 
@@ -14,7 +12,28 @@ function Login() {
         <Grid container>
           <Grid item xs={12} sm={6} sx={{alignItems: 'center', display: 'flex' , height: '100vh'}}>
             <Container maxWidth="sm">
-             <h1>Login</h1>
+            <Box sx={{ marginBottom: pxToRem(24)}}>
+              <Logo height={41} width={100}/>
+              </Box>
+
+              <Box sx={{ marginBottom: pxToRem(24)}}>
+              <StyledH1>Bem-Vindo</StyledH1>
+              <Styledp>Digite sua senha para logar</Styledp>
+              </Box>
+             
+             <FormComponent input={[
+              {type: 'email' , placeholder: 'Email', disabled: true},
+              {type: 'password' , placeholder: 'Senha'},
+             ]} 
+             
+             buttons={[
+              {className: 'primary' , type: 'submit' , children: 'Login', disabled: true},
+             ]}
+             
+             message={{
+              msg: 'Sucesso !',
+              type: 'success' ,
+             }}/>
             </Container>
             </Grid>
 
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,6 +1,7 @@
 
 import { Box, Container, Grid } from "@mui/material";
-import { BannerImage } from "@/components";
+import { BannerImage, FormComponent, Logo, StyledH1, Styledp, StyledUl } from "@/components";
+import { pxToRem } from "@/utils";
 
 
 function Registration() {
@@ -13,7 +14,35 @@ function Registration() {
         <Grid container>
           <Grid item xs={12} sm={6} sx={{alignItems: 'center', display: 'flex' , height: '100vh'}}>
             <Container maxWidth="sm">
-            <h1>Cadastro</h1>
+
+            <Box sx={{ marginBottom: pxToRem(24)}}>
+            <Logo height={41} width={100}/>
+              </Box>
+
+           <Box sx={{ marginBottom: pxToRem(24)}}>
+              <StyledH1>Faça seu cadastro</StyledH1>
+              <Styledp>Primeiro, diga-nos quem voce é</Styledp>
+              <StyledUl>
+                <li>Entre 8 e 16 caracteres</li>
+                <li>Pelo menos uma letra maisucula</li>
+                <li>Pelo menos um caracter especial</li>
+                <li>Pelo menos um numero</li>
+              </StyledUl>
+            </Box>
+             
+            <FormComponent input={[
+              {type: 'email' , placeholder: 'Email'},
+              {type: 'password' , placeholder: 'Senha'},
+             ]} 
+             
+             buttons={[
+              {className: 'primary' , type: 'submit' , children: 'Login'},
+             ]}
+             
+             message={{
+              msg: 'ERRO!',
+              type: 'error' ,
+             }}/>
             </Container>
             </Grid>
 
