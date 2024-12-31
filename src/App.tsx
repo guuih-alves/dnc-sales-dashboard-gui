d7d417e8ac45bba2416d652bf00bb4b257293737
Guilherme Alves <guilherme_alves_08@hotmsil.com>
2024-12-31 15:14:33 -0300

    Feat: login Page request

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from "react-router-dom"
+import Cookies from "js-cookie"
 import { Home, Leads, Profile, Login, Registration } from "./pages"
 
 function App() {
+  const ProtectedRoute = () =>{
+    const checkAuthCookie = Cookies.get('Authorization')
+    if (!checkAuthCookie) {
+      alert('Autenticacao necessaria')
+      return <Navigate to='/' replace />
+    }
 
+    return <Outlet />
+
+  }
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login/>}></Route>
         <Route path="/cadastro" element={<Registration />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/leads" element={<Leads />}></Route>
-        <Route path="/perfil" element={<Profile />}></Route>
+        <Route element={<ProtectedRoute/> }>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/leads" element={<Leads />}></Route>
+          <Route path="/perfil" element={<Profile />}></Route>
+        
+        </Route>
+     
       </Routes>
     </Router>
   )
